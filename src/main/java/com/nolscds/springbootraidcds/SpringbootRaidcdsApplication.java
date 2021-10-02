1d15e05379a0911ae1caeb46def003dd63badb1b
Marian-Daniel Rolník <rolnik.d@gmail.com>
2021-10-02 21:42:39 +0200

    Initial commit

---
+++ src/main/java/com/nolscds/springbootraidcds/SpringbootRaidcdsApplication.java
@@ -0,0 +1,13 @@
+package com.nolscds.springbootraidcds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRaidcdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRaidcdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nolscds/springbootraidcds/SpringbootRaidcdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolscds.springbootraidcds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRaidcdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
